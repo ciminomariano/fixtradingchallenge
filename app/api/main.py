da0559b45205c70fe6807b1d3746c91fc6fe1f8d
mariano <ciminomariano@gmail.com>
2023-07-12 00:58:38 -0300

    Change on Procfile

--- app/api/main.py
+++ app/api/main.py
@@ -9,10 +9,6 @@ from endpoints.routes import router as delivery_router
 import uvicorn
 import os
 
-api_dir = os.path.join(os.path.dirname(__file__), 'app', 'api')
-
-# Añade la ruta al sys.path
-sys.path.append(api_dir)
 
 port = int(os.environ.get("PORT", 8000))
 
